27b0805dbeda2bf962d1667c7d5fad3431104bfa
xiaohuoni <448627663@qq.com>
2019-09-03 14:18:18 +0800

    npm publish 0.0.4

--- template/DragDropComponent.js
+++ template/DragDropComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DragDropContext } from "react-dnd";
 const DropContext = ({ children }) => {
-  return <div>123123{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default DragDropContext(HTML5Backend)(DropContext);
